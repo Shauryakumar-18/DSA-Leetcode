ee309c892417d60842bff34037364d4a6792b8b2
Shauryakumar-18 <shauryakumar200618@gmail.com>
2026-09-02 15:00:58 +0900

    Time: 0 ms (100%), Space: 21.1 MB (64.71%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int count=0;
+       
+        for(int i=0;i<batteryPercentages.size();i++){
+             
+            
+            if(batteryPercentages[i]-count>0){
+                count++;
+
+            }
+
+            
+        }
+        return count;
+        
+    }
+};
